27ae6267db5591c637f71bc1bc3d467f07b975c5
websupergirl <nikol@thatsupergirl.com>
2022-01-12 00:54:36 -0800

    fix page location issue

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import React from "react";
+import StartAtTop from "./components/StartAtTop";
+import { BrowserRouter } from "react-router-dom";
 import DdadMenu from "./components/Navbar.js";
+import { Stack } from "react-bootstrap";
 import DdadRoutes from "./components/Routes.js";
 import DdadFooter from "./components/Footer.js";
 import DdadCopyright from "./components/Copyright.js";
-import { Stack } from "react-bootstrap";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <DdadMenu />
-        <Stack gap={5}>
-          <div>
-            <DdadRoutes />
-          </div>
-          <div>
-            <DdadFooter />
-          </div>
-          <div>
-            <DdadCopyright />
-          </div>
-        </Stack>
+        <StartAtTop>
+          <DdadMenu />
+          <Stack gap={5}>
+            <div>
+              <DdadRoutes />
+            </div>
+            <div>
+              <DdadFooter />
+            </div>
+            <div>
+              <DdadCopyright />
+            </div>
+          </Stack>
+        </StartAtTop>
       </BrowserRouter>
     </div>
   );
